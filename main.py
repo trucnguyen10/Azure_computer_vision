178c119137c1c01a8681e84931e8aa1cf0229691
trucnguyen <trucnguyen@gmail.com>
2023-04-26 21:07:28 -0400

    object version

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from msrest.authentication import CognitiveServicesCredentials
 
 from array import array
 import os
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import sys
 import time
 
@@ -18,33 +18,53 @@ computervision_client = ComputerVisionClient(
 # remote_image_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"
 folder = 'C:/Users/hersa/ComputerVision'
 files = os.listdir(folder)
+font = ImageFont.truetype('arial.ttf', 16)
+
 for file in files:
     file_path = os.path.join(folder, file)
-
+    image = Image.open(file_path)
+    image_draw = ImageDraw.Draw(image)
     with open(file_path, mode='rb') as image_stream:
-        description_results = computervision_client.describe_image_in_stream(
-            image_stream)
-        print("Description of remote iamge: ")
-        if (len(description_results.captions) == 0):
-            print("No description detected")
-        else:
-            for caption in description_results.captions:
-                print("'{}' with confidence {:.2f}".format(
-                    caption.text, caption.confidence))
-# print("===== Tag an image - remote =====")
-# # Call API with remote image
-# tags_result_remote = computervision_client.tag_image(remote_image_url)
+        results = computervision_client.detect_objects_in_stream(image_stream)
 
-# # Print results with confidence score
-# print("Tags in the remote image: ")
-# if (len(tags_result_remote.tags) == 0):
-#     print("No tags detected.")
-# else:
-#     for tag in tags_result_remote.tags:
-#         print("'{}' with confidence {:.2f}%".format(
-#             tag.name, tag.confidence * 100))
-# print()
-# '''
-# END - Tag an Image - remote
-# '''
-# print("End of Computer Vision quickstart.")
+        for object in results.objects:
+            left = object.rectangle.x
+            top = object.rectangle.y
+            width = object.rectangle.w
+            height = object.rectangle.h
+
+            shape = [(left, top), (left + width, top + height)]
+            image_draw.rectangle(shape, outline='red', width=5)
+            text = f'{ object.object_property } ({object.confidence * 100}%)'
+            image_draw.text((left + 5 - 1, top + height - 30 + 1),
+                            text, (0, 0, 0), font=font)
+            image_draw.text((left + 5, top + height - 30),
+                            text, (255, 0, 0), font=font)
+
+        image.show()
+#         description_results = computervision_client.describe_image_in_stream(
+#             image_stream)
+#         print("Description of remote iamge: ")
+#         if (len(description_results.captions) == 0):
+#             print("No description detected")
+#         else:
+#             for caption in description_results.captions:
+#                 print("'{}' with confidence {:.2f}".format(
+#                     caption.text, caption.confidence))
+# # print("===== Tag an image - remote =====")
+# # # Call API with remote image
+# # tags_result_remote = computervision_client.tag_image(remote_image_url)
+
+# # # Print results with confidence score
+# # print("Tags in the remote image: ")
+# # if (len(tags_result_remote.tags) == 0):
+# #     print("No tags detected.")
+# # else:
+# #     for tag in tags_result_remote.tags:
+# #         print("'{}' with confidence {:.2f}%".format(
+# #             tag.name, tag.confidence * 100))
+# # print()
+# # '''
+# # END - Tag an Image - remote
+# # '''
+# # print("End of Computer Vision quickstart.")
